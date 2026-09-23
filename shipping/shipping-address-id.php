d0bfca948aec2b70c5cc1c838aabfbb0d4fb9787
RollyCBS11 <cbs.webdesign11@gmail.com>
2026-09-24 02:10:50 +0800

    gulp all

--- shipping/shipping-address-id.php
+++ shipping/shipping-address-id.php
@@ -34,8 +34,8 @@ $useTabbedLocationPicker = true;
 
 <?php if ($useTabbedLocationPicker): ?>
 
-<link rel="preload" as="style" href="<?= REPONAME ?>/src/common/css/location-picker-tabs.css?t=1790183223572">
-<link rel="stylesheet" href="<?= REPONAME ?>/src/common/css/location-picker-tabs.css?t=1790183223572">
+<link rel="preload" as="style" href="<?= REPONAME ?>/src/common/css/location-picker-tabs.css?t=1790187023119">
+<link rel="stylesheet" href="<?= REPONAME ?>/src/common/css/location-picker-tabs.css?t=1790187023119">
 
 <div class="mb-3">
     <label class="p cart-input-label" for="location_picker_display_id"><?= $collector_sh->translate("location_label".$country, "Province, City / Regency, District, Village"); ?></label>
@@ -346,7 +346,7 @@ $useTabbedLocationPicker = true;
 </script>
 
 <?php if ($useTabbedLocationPicker): ?>
-    <script src="<?= REPONAME ?>/src/common/js/location-picker-tabs.js?t=1790183223572" defer></script>
+    <script src="<?= REPONAME ?>/src/common/js/location-picker-tabs.js?t=1790187023119" defer></script>
 <?php endif; ?>
 <?php $collector_sh->saveTranslation(); ?>
 
